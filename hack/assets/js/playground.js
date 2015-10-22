671f6767e0767aa9837e6f806705b83fbe17ad50
J.M <jm@jublo.net>
2015-10-22 13:36:43 +0200

    Finish side-by-side comparison

--- hack/assets/js/playground.js
+++ hack/assets/js/playground.js
@@ -7,16 +7,17 @@ jQuery(document).ready(function() {
 
     /* Remove fonts that are not webfonts and are not installed */
     var removeUnavailableFonts = function () {
-        var select = $('#face option'), option, face;
+        var select = $("select.face option"), option, face;
         // skip first entry = Hack,
         // never remove it even when it's not finished loading yet
         for (var i = 1; i < select.length; i++) {
             option = select.eq(i);
             face   = option.text();
             if (!isFontAvailable(face)) {
-                if (typeof option.attr('data-webfont') === "undefined") {
+                if (typeof option.attr("data-webfont") === "undefined") {
                     // no web font available, we must remove this font
                     option.remove();
+                    console.info("Removing " + face + ".");
                 }
             }
         }
@@ -25,74 +26,116 @@ jQuery(document).ready(function() {
 
     /* Editor controls */
     ace.config.set("basePath", "https://cdnjs.cloudflare.com/ajax/libs/ace/1.2.0");
-    var editor = ace.edit("playground");
-    editor.renderer.setScrollMargin(20);
-    editor.getSession().setUseWorker(false);
+
+    var editors = [];
+    var aceInit = function (index) {
+        editors[index] = ace.edit("playground" + index);
+        editors[index].renderer.setScrollMargin(20);
+        editors[index].$blockScrolling = Infinity;
+        editors[index].getSession().setUseWorker(false);
+    };
+    aceInit(1);
+    aceInit(2);
 
     // More info link open in new tab
-    $('#moreinfo a').prop('target', '_blank');
+    $("#moreinfo a").prop("target", "_blank");
 
-    $('#face').change(function (ev) {
+    // clone font face control
+    $(".styled-select.face.editor2 select")
+        .replaceWith($(".styled-select.face.editor1 select").clone());
+
+    // sync editors
+    editors[2].setSession(editors[1].getSession());
+
+    var first_run = true;
+    $(document).on("change", "select.face", function (ev) {
+        var editorIndex = $(this).parent().hasClass("editor2") ? 2 : 1;
         var face = $(this).val();
         $(this).parent().find("span").text(face);
         if (!isFontAvailable(face)
             && face !== 'Hack'
-        ) {
+            ) {
             // instruct the loader to fetch this font for us
-            var webfont     = $(this).find("option:selected").attr('data-webfont'),
+            var webfont = $(this).find("option:selected").attr("data-webfont"),
                 face_folder = face.toLowerCase().split(" ").join("-"),
-                config      = webfont === "custom"
+                config = webfont === "custom"
                     ? {
-                            custom: {
-                                families: [face],
-                                urls: ['assets/fonts/' + face_folder + '/webfont.css']
-                            }
+                        custom: {
+                            families: [face],
+                            urls: ["assets/fonts/" + face_folder + "/webfont.css"]
                         }
+                    }
                     : {
-                            google: {
-                                families: [face]
-                            }
-                        };
+                        google: {
+                            families: [face]
+                        }
+                    };
             WebFont.load(config);
         }
-        editor.setOptions({
-            fontFamily: face
-        });
+        if (first_run) {
+            console.info("Applying face change to all editors");
+            for (var i = 1; i < editors.length; i++) {
+                editors[i].setOptions({
+                    fontFamily: face
+                });
+            }
+            first_run = false;
+        } else {
+            console.info("Changing editor " + editorIndex + " to " + face);
+            editors[editorIndex].setOptions({
+                fontFamily: face
+            });
+        }
 
         // update more info text
-        var href = $(this).find("option:selected").attr('data-href');
-        $('#moreinfo')
-        .find('span').text(face).end()
-        .find('a').prop('href', href).text(href).end()
-        .css('visibility', 'visible');
-    })
-    .trigger("change");
-    $('#size').change(function (ev) {
+        var href = $(this).find("option:selected").attr("data-href");
+        $("#moreinfo")
+            .find("span").text(face).end()
+            .find("a").prop("href", href).text(href).end()
+            .css("visibility", "visible");
+    });
+    $("select.face").trigger("change");
+    $("select.size").change(function (ev) {
         var size = $(this).val();
         $(this).parent().find("span").text(size);
-        editor.setOptions({
-            fontSize: size + "pt"
-        });
+        for (var i = 1; i < editors.length; i++) {
+            editors[i].setOptions({
+                fontSize: size + "pt"
+            });
+        }
     })
     .trigger("change");
-    $('#mode').change(function (ev) {
+    $("select.mode").change(function (ev) {
         var mode = $(this).val(),
             displayMode = $(this).find("option:selected").text();
         $(this).parent().find("span").text(displayMode);
-        editor.getSession().setMode(mode);
+        editors[1].getSession().setMode(mode);
     })
     .trigger("change");
-    $('#theme')
-    .val((localStorage && localStorage.playgroundEditorTheme)
-        ? localStorage.playgroundEditorTheme
-        : 'ace/theme/eclipse'
-    )
-    .change(function (ev) {
+    $("select.theme").change(function (ev) {
         var theme = $(this).val(),
             displayTheme = $(this).find("option:selected").text();
         $(this).parent().find("span").text(displayTheme);
         localStorage.playgroundEditorTheme = theme;
-        editor.setTheme(theme);
+        for (var i = 1; i < editors.length; i++) {
+            editors[i].setTheme(theme);
+        }
     })
+    .val((localStorage && localStorage.playgroundEditorTheme)
+        ? localStorage.playgroundEditorTheme
+        : "ace/theme/eclipse"
+    )
     .trigger("change");
+
+    /* full screen */
+    (function () {
+        $("a#enter-fullscreen, a#exit-fullscreen").click(function (e) {
+            e.preventDefault();
+            $(document.body).toggleClass("fullscreen");
+            $(window).trigger("resize");
+        });
+        $(window).resize(function () {
+            $("#editors").css("top", $("#editor-controls").height() + 5);
+        });
+    })();
 });
